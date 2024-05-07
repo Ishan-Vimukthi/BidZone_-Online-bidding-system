d4cc8b38b3660714f356339b63be301c43acf8f7
Ishan-Vimukthi <ishanvimukthie@gmail.com>
2024-05-07 11:03:20 +0530

    file struct

---
+++ BidZone_Online-bidding-system/src/main/java/com/group1/BidZone_Onlinebiddingsystem/BidZoneOnlineBiddingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.group1.BidZone_Onlinebiddingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidZoneOnlineBiddingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidZoneOnlineBiddingSystemApplication.class, args);
+	}
+
+}
---
+++ BidZone_Online-bidding-system/src/test/java/com/group1/BidZone_Onlinebiddingsystem/BidZoneOnlineBiddingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group1.BidZone_Onlinebiddingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidZoneOnlineBiddingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
