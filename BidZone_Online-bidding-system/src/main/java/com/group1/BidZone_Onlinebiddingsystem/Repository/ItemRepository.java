e62ff6a718e423a7e62f6559a532c56f6b792728
Ishan-Vimukthi <ishanvimukthie@gmail.com>
2024-05-10 14:42:30 +0530

    up

--- BidZone_Online-bidding-system/src/main/java/com/group1/BidZone_Onlinebiddingsystem/Repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.group1.BidZone_Onlinebiddingsystem.Repository;
-
-import com.group1.BidZone_Onlinebiddingsystem.Model.Item;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepository extends CrudRepository<Item, Long> {
-}
--- BidZone_Online-bidding-system/src/main/java/com/group1/BidZone_Onlinebiddingsystem/Service/ItemService.java
+++
@@ -1,9 +0,0 @@
-package com.group1.BidZone_Onlinebiddingsystem.Service;
-
-import com.group1.BidZone_Onlinebiddingsystem.Model.Item;
-
-public interface ItemService {
-    Item addItem(Item item);
-    // Other methods as needed
-}
-
